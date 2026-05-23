90b504b89ec0da73de095702ee533693e73ff36f
Nilesh Dave <nileshdave1511@gmail.com>
2026-05-23 21:48:35 +0530

    font

--- trikal_portal/settings.py
+++ trikal_portal/settings.py
@@ -71,6 +71,7 @@ WSGI_APPLICATION = 'trikal_portal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#database
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
@@ -112,7 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # यह लाइन बहुत जरूरी है
+
 
 # 🚀 यह नई लाइन जोड़नी है ताकि जांगो को CSS मिल जाए
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
