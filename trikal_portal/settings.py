29f8ea5c1ee43b392c34c3029c07e186b3ac5876
Nilesh Dave <nileshdave1511@gmail.com>
2026-05-23 21:05:07 +0530

    Switch to PostgreSQL

--- trikal_portal/settings.py
+++ trikal_portal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -70,15 +70,13 @@ WSGI_APPLICATION = 'trikal_portal.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/6.0/ref/settings/#database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
